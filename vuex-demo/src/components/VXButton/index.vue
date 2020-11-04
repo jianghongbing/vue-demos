35da697d9ddb82e04fa4a9adf1eed82a305b8a03
jianghongbing <jianghongbing1989@qq.com>
2020-11-04 14:18:52 +0800

    add count-demo-01

---
+++ vuex-demo/src/components/VXButton/index.vue
@@ -0,0 +1,23 @@
+<template>
+ <button><slot></slot></button>
+</template>
+
+<script>
+export default {
+ name: 'vx-button',
+}
+</script>
+
+<style scoped>
+ button {
+  background: #42b983;
+  color: #fff;
+  font-size: 15px;
+  font-weight: bolder;
+  padding: 10px;
+  min-width: 200px;
+  border: none;
+  border-radius: 10px;
+  outline: none;
+ }
+</style>
